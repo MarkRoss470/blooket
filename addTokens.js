d4e653ed5723cb6f4baaec1c937590ab02586941
Mark Ross <markross470@gmail.com>
2023-01-29 14:34:13 +0000

    Fixed for API change Plugin no longer requests the number of tokens the user has left, it just requests the max amount of tokens and XP

--- addTokens.js
+++ addTokens.js
@@ -1,7 +1,7 @@
-//adds currencies using endpoint /api/users/add-rewards
+// Adds currencies using endpoint /api/users/add-rewards
 export async function addTokens() {
 
-	//blooket overrides the default window.alert function, so this is needed to access it
+	// Blooket overrides the default window.alert function, so this is needed to access it
 	function customAlert(msg)
 	{
 		let appdiv = document.getElementById("app");
@@ -10,45 +10,15 @@ export async function addTokens() {
 		iframe.contentWindow.alert(msg);
 		iframe.remove();
 	}
-
-	//fetches daily reward info from endpoint /api/users/bonuses
-	//used for addedTokens and addedXp fields of payload (so that the program does not request more than the quotas per day)
-	async function getRewardInfo() {
-		const response = await fetch('https://play.blooket.com/api/users/bonuses', {
-			method: "GET",
-			headers: {
-				"accept": "application/json, text/plain, */*",
-				"accept-language": "en-US,en;q=0.9,ru;q=0.8",
-			},
-			credentials: "include"
-		});
-		return await response.json();
-	};
-
-	let rewardData = await getRewardInfo();
-	
-	if(rewardData.tokensAvailable == undefined || rewardData.xpAvailable == undefined)
-	{
-		customAlert("Error collecting user info");
-		return;
-	}
-	
-	//no point sending request if already at max tokens and xp
-	if(!(rewardData.tokensAvailable || rewardData.xpAvailable))
-	{
-		customAlert("Already at max tokens and xp");
-		return;	
-	}
 	
 	let JSONBody = {
-		addedTokens:rewardData.tokensAvailable,
-		//addedTokens:10,
-		addedXp:rewardData.xpAvailable
+		addedTokens: 500,
+		addedXp: 300
 	};
 
 	let payload = (new TextEncoder).encode(JSON.stringify(JSONBody));
 
-	//send request to blooket api
+	// Send request to blooket API
 	const response = await fetch('https://play.blooket.com/api/users/add-rewards', {
 		method: "PUT",
 		headers: {
@@ -58,13 +28,16 @@ export async function addTokens() {
 		body: payload
 	});
 	
+	console.log(response)
+
 	if(response.status != 200)
 	{
 		customAlert("Error adding tokens");
 		return;
 	}
 	
-	//reload page to sync changes with server
+	// Reload page to sync changes with server
+	// This seems to be broken at the moment
 	location.reload();
 	
 };
