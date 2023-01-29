ce5f111e22a371154af698cb82864facca2c83f0
Mark Ross <markross470@gmail.com>
2023-01-29 15:43:37 +0000

    Removed blook changing functionality Blook changing functionality has not worked in a while, so I removed it.

--- popup.js
+++ popup.js
@@ -1,13 +1,6 @@
 import {addTokens} from "./addTokens.js";
-import {allBlooks} from "./blooks.js";
-
 
 let addTokensButton = document.getElementById("addtokens");
-let changeBlookButton = document.getElementById("changeblook");
-let blookNameInput = document.getElementById("blookname");
-let usernameInput = document.getElementById("username");
-let blooksDivParent = document.getElementById("blooks");
-let kickButton = document.getElementById("kick");
 
 addTokensButton.addEventListener("click", async () => {
 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -24,55 +17,3 @@ addTokensButton.addEventListener("click", async () => {
 		function: addTokens
 	});
 });
-
-async function changeBlook(blook=null)
-{
-	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-	let parsedUrl = new URL(tab.url);
-	console.log(parsedUrl);
-	if(parsedUrl.hostname != "dashboard.blooket.com" || parsedUrl.pathname != "/play/lobby")
-	{
-		//TODO: change to a nicer message format
-		alert("You must be in a lobby to use this function");
-		return
-	}
-	chrome.tabs.sendMessage(tab.id, {
-		text: "change_blook", 
-		blook: blook, 
-		username: usernameInput.value == "" ? undefined : usernameInput.value
-	});
-}
-
-changeBlookButton.addEventListener("click", (e) => {changeBlook(blookNameInput.value)});
-
-kickButton.addEventListener("click", (e) => {
-	if(usernameInput.value == "" && !confirm("kick yourself?"))return;
-	changeBlook(null);
-});
-
-function loadBlooks(){
-	let blooksDiv = document.createElement("div");
-	for(let blookname in allBlooks)
-	{
-		let blookIcon = document.createElement("img");
-		blookIcon.id = "blookicon-" + blookname;
-		blookIcon.setAttribute("src", allBlooks[blookname]);
-		blookIcon.setAttribute("class", "blookIcon");
-		blookIcon.setAttribute("loading", "lazy");
-		blookIcon.addEventListener("click", (e) => {
-			blookIcon.style['background-color'] = "purple";
-			try
-			{
-				document.getElementById("blookicon-" + blookNameInput.value).style['background-color'] = "white";
-			}
-			catch{}
-			blookNameInput.value = blookname;
-		});
-
-		blooksDiv.appendChild(blookIcon);
-	}
-	blooksDivParent.appendChild(blooksDiv);
-}
-requestIdleCallback(loadBlooks);
-
-
